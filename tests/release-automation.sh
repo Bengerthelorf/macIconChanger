0d44dcd6360d98e5f477a11c27bdf0329e6d6a18
Bengerthelorf <bengerlorf@hotmail.com>
2026-07-28 21:58:12 +0800

    ci: allow release workflow to redeploy docs

--- tests/release-automation.sh
+++ tests/release-automation.sh
@@ -95,6 +95,8 @@ fi
 
 /usr/bin/grep -Fq "workflow_dispatch:" "$PUBLISH_WORKFLOW" ||
     fail "publishing must require a manual dispatch"
+/usr/bin/grep -Fq "actions: write" "$PUBLISH_WORKFLOW" ||
+    fail "publishing must be allowed to dispatch the docs workflow"
 /usr/bin/grep -Fq "candidate_run_id" "$PUBLISH_WORKFLOW" ||
     fail "publishing must promote a specific tested candidate"
 /usr/bin/grep -Fq "I_APPROVE_RELEASE" "$PUBLISH_WORKFLOW" ||
